d90019830a6db2aff126864b187ab922d788423f
martin.gubler <martingubler04@gmail.com>
2024-11-21 23:45:33 -0300

    sancando cors config

--- src/server.js
+++ src/server.js
@@ -14,13 +14,8 @@ import ProductRepository from './repositories/product.repository.js';
 const app = express();
 const PORT = ENVIROMENT.PORT || 3000
 
-const allowedOrigins = ['https://pwa-noviembre-proyecto-desplegado-frontend-sigma.vercel.app'];
 
-app.use(cors({
-    origin: allowedOrigins, // Permitir solo solicitudes desde tu dominio frontend
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP permitidos
-    credentials: true, // Permitir envío de cookies o cabeceras con credenciales}
-}));
+app.use(cors());
 app.use(express.json({limit: '5mb'}))
 app.use(verifyApiKeyMiddleware)
 
